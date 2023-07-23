0ee3d6565f3144a908b7ef93f7a6c9e2d45b207b
ROHIT-01234 <120010829+ROHIT-01234@users.noreply.github.com>
2023-07-23 18:34:25 +0530

    Create IntFloat Sort.cpp

---
+++ IntFloat Sort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+template <class T>void bubble_sort(T*arr,int n)
+{   
+    T temp;
+     for (int j = 0; j < n; j++)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[i - 1] > arr[i])
+                {
+                    temp = arr[i - 1];
+                    arr[i - 1] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            cout << arr[i] << endl;
+        }
+};
+
+int main()
+{
+
+    int arr[4]={3,5,4,2};
+    bubble_sort(arr,4);
+
+    float arra[4]={1.1,2.3,0.1,-1.0};
+    bubble_sort (arra,4);
+
+    return 0;
+}
